9a016931f307073b581b0f22e836f4b09407cdd8
Carlos Mendes <cmendesce@gmail.com>
2024-08-17 16:03:06 -0300

    adding support to pass backend port

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Substitua o placeholder pelo valor da variável de ambiente
 sed -i "s/FAULT_PERCENTAGE_PLACEHOLDER/${FAULT_PERCENTAGE:-100}/" /etc/envoy/envoy.yaml
-
-# Inicie o Envoy
+sed -i "s/PORT_PLACEHOLDER/${BACKEND_PORT:-80}/" /etc/envoy/envoy.yaml
 exec envoy -c /etc/envoy/envoy.yaml
